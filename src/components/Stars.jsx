ff12c4f7a7d9ee5e668e76787a9efdc3bc48347d
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-01-12 18:19:54 +0100

    add stelle vuote

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -1,12 +1,25 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faStar } from "@fortawesome/free-solid-svg-icons"
+import { faStar as faRegularStar } from '@fortawesome/free-regular-svg-icons';
+
 
 export default function Stars({ avg_vote }) {
   const arraystar = []
-  const vote = avg_vote.vote || avg_vote;
+  const vote = Math.ceil(avg_vote);
+
   for (let index = 0; index < vote; index++) {
-    arraystar.push(<FontAwesomeIcon icon={faStar} style={{ color: "gold" }} key={index} />)
+    arraystar.push(
+      <FontAwesomeIcon icon={faStar} style={{ color: "gold" }} key={`full-${index}`} />
+    );
   }
+
+  // 2 ciclo per aggiungere stella vuota
+  for (let index = vote; index < 5; index++) {
+    arraystar.push(
+      <FontAwesomeIcon icon={faRegularStar} style={{ color: "gold" }} key={`empty-${index}`} />
+    );
+  }
+
 
   return (
     <div className="mb-2 mt-n2">
