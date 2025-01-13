dbcf147feb060800e296ac02f2532062b320c482
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-01-13 16:06:05 +0100

    add controlli al form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -20,25 +20,41 @@ export default function Form() {
 
   function handleSubmit(e) {
     e.preventDefault()
-    console.log("Dati inviati:", formData);
+    const dataName = formData.name.trim()
+    const vote = parseInt(formData.vote)
+
+    // Validazione
+    if (!dataName || dataName.length > 255 || !vote || vote < 1 || vote > 5 || formData.text.length > 255) {
+      console.log("Il modulo non è valido",);
+      return;
+    }
+
+    console.log("Dati inviati:", formData.text.length, {
+      ...formData,
+      name: dataName
+
+    });
+
+    setFormData(initialFormData)
   }
 
   return (
     <form className="d-flex flex-column mb-3" onSubmit={handleSubmit} >
+      <label className="text-center">Scrivi una recensione!</label>
       <div>
-        <label htmlFor="nome" className="form-label">Nome</label>
+        <label htmlFor="nome" className="form-label">Nome*</label>
         <input required
           type="text" className="form-control" id="nome" name="name" value={formData.name} onChange={handleChange} />
       </div>
 
-      <div className="mb-3">
-        <label htmlFor="descrizione" className="form-label">Inserisci una descrizione</label>
-        <textarea className="form-control" id="descrizione" name="text" rows="3" value={formData.text} onChange={handleChange} ></textarea>
+      <div className="mt-2">
+        <label htmlFor="descrizione" className="form-label">Inserisci una descrizione </label>
+        <textarea className="form-control" id="descrizione" name="text" rows="3" placeholder="Massimo 255 caratteri" maxLength={255} value={formData.text} onChange={handleChange} ></textarea>
       </div>
 
-      <div className="mb-3">
-        <label htmlFor="voto" className="form-label" >Seleziona un voto</label>
-        <select className="form-select" id="voto" name="vote" value={formData.vote} onChange={handleChange}>
+      <div className="mt-2">
+        <label htmlFor="voto" className="form-label" >Seleziona un voto*</label>
+        <select required className="form-select" id="voto" name="vote" value={formData.vote} onChange={handleChange}>
 
           <option value="">Seleziona un voto</option>
           <option value="1">1</option>
